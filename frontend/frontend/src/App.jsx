66625483cd0c57144e9763d2d745ec66f9654ad5
User <User@WRSD-LD309-018.dut.ac.za>
2024-09-09 11:27:37 +0200

    UI-Design

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
+import Home from './components/home'
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-    
+    <Home/>
     </>
   )
 }
